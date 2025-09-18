848b8bdcfa43725bed0bff361c3b1b385881687e
leirerojas <rojasleire@gmail.com>
2025-09-18 11:29:52 -0300

    funcion para lista de amigos

--- app.js
+++ app.js
@@ -9,5 +9,14 @@ function agregarAmigo(){
    amigos.push(nombre);
    document.getElementById("amigo").value = "";
    console.log(amigos);
+   listaDeAmigos();
+}
+
+function listaDeAmigos(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++){
+        lista.appendChild(`<li> ${amigos[i]} </li>`);
+    }
 }
 
