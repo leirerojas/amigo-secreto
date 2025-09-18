a9a26b460c5c47b101b8b00b2c1750ed6515f0d9
leirerojas <rojasleire@gmail.com>
2025-09-18 11:36:33 -0300

    Ajuste para agregar elemento a lista

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function listaDeAmigos(){
     let lista = document.getElementById("listaAmigos");
     lista.innerHTML = "";
     for(let i = 0; i < amigos.length; i++){
-        lista.appendChild(`<li> ${amigos[i]} </li>`);
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
     }
 }
 
